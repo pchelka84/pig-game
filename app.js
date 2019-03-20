c9dcc59f296b1a2ab62fbbee73fccd67e755524a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-03-20 11:35:56 -0400

    Changing active player

--- app.js
+++ app.js
@@ -42,8 +42,11 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
     roundScores = 0;
 
-    document.getElementById("currnet-0").textContent = "0";
-    document.getElementById("currnet-1").textContent = "0";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
   }
 });
 
