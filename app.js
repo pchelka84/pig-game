9041ce1a89a76566acb09c39866c07b8d53d47c2
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-03-20 11:17:54 -0400

    Setting scores to 0, displaying dice values dynamically

--- app.js
+++ app.js
@@ -9,20 +9,35 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-// console.log(dice);
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3. Update the round score IF the rolled number was not a 1
+});
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
-document.querySelector("#current-" + activePlayer).innerHTML =
-  "<em>" + dice + "</em>";
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "</em>";
 
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
-
-document.querySelector(".dice").style.display = "none";
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
