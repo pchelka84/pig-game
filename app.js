22ed5bd587bc190e5c212581c3e2ed6ba82e3c00
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-03-20 11:29:54 -0400

    Setting current score to 0 when '1' is rolled

--- app.js
+++ app.js
@@ -40,6 +40,10 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   } else {
     // change the player
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScores = 0;
+
+    document.getElementById("currnet-0").textContent = "0";
+    document.getElementById("currnet-1").textContent = "0";
   }
 });
 
