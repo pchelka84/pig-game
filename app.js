2677ea15b76b7285a2a52ee6813461286dda7f94
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-03-20 11:25:19 -0400

    Changing player when the dice = 1

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   diceDOM.src = "dice-" + dice + ".png";
 
   // 3. Update the round score IF the rolled number was not a 1
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // change the player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  }
 });
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
