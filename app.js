20e301808da04fa0fab4016af098fc0c3d3934db
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2019-03-20 10:53:24 -0400

    Hiding dice at the beginning of the game

--- app.js
+++ app.js
@@ -18,4 +18,11 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+document.querySelector("#current-" + activePlayer).innerHTML =
+  "<em>" + dice + "</em>";
+
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
